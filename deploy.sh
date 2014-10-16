18346703c87ee962a608ba3378f4547a740c2793
Pavel Derendyaev <paul@reic.ru>
2014-10-16 14:09:50 +0400

    rebuilding site Thu Oct 16 14:09:50 MSK 2014

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 # Build the project.
 hugo -t hyde
 
-# Add changes to git.
-git add -A
-
 # Till https://github.com/spf13/hugo/issues/230 will be fixed completely
 #mkdir -p public/blog
 #mv public/20** public/blog/
 mv public/blog/* public/
 rmdir public/blog
+
+# Add changes to git.
+git add -A
 
 # Commit changes.
 msg="rebuilding site `date`"
